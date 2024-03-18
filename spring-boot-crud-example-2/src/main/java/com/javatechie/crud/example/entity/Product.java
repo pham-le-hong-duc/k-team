7417efdc6a46dae0cb94b7b1b9108be05fc9dd81
pham-le-hong-duc <honghongduc0102@gmail.com>
2024-03-18 23:30:38 +0700

    Init spring CRUD v1

---
+++ spring-boot-crud-example-2/src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -0,0 +1,21 @@
+package com.javatechie.crud.example.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "PRODUCT_TBL")
+public class Product {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int quantity;
+    private double price;
+}
